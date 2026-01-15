048fe4f926b82f28a3c334e45efbb068e75b7757
Amn4583 <nnerror811@gmail.com>
2026-01-15 11:46:56 +0530

    Clear repository content

--- Scripts/Autoloads/EventsManager.gd
+++
@@ -1,7 +0,0 @@
-# GlobalSignals.gd
-extends Node
-
-signal custom_event(data)  # You can name it anything
-
-func emit_custom_event(data):
-	emit_signal("custom_event", data)
--- Scripts/Minigames/paper.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-@onready var label = $Label3D
-
-func set_code(code_text):
-	label.text = code_text
-
-	
